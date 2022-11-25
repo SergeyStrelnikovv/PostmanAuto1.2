8b1f66abf0292c9f5f442bf3049b1e1602032953
SergeyStrelnikovv <94080285+SergeyStrelnikovv@users.noreply.github.com>
2022-11-25 12:13:40 +0300

    Test fail

--- src/test/java/ru/netology/test/PostmanTest.java
+++ src/test/java/ru/netology/test/PostmanTest.java
@@ -22,7 +22,7 @@ public class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Стрельников"))
+                .body("data", equalTo("Стрельникав"))
         ;
     }
 
