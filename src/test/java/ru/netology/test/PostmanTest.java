5d4933d92d3b6d52ea778504fedc2988877def4d
SergeyStrelnikovv <94080285+SergeyStrelnikovv@users.noreply.github.com>
2022-11-25 12:15:00 +0300

    Update PostmanTest.java

--- src/test/java/ru/netology/test/PostmanTest.java
+++ src/test/java/ru/netology/test/PostmanTest.java
@@ -22,7 +22,7 @@ public class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Стрельникав"))
+                .body("data", equalTo("Стрельников"))
         ;
     }
 
